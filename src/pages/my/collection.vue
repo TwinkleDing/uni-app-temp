77a11d712cf8f0344c1b971df83a75ba337f01e5
TinkleDing <twinkle415@163.com>
2020-03-29 10:38:46 +0800

    add collection

---
+++ src/pages/my/collection.vue
@@ -0,0 +1,165 @@
+<template>
+	<view class="index">
+		<swiper @change="swpierChange" :style="{height:screenHeight + 'px'}">
+			<swiper-item v-for="(value,index) in data" :key="index" @click="preImg(index)">
+				<image :src="value" mode="widthFix"></image>
+			</swiper-item>
+		</swiper>
+		<!-- #ifndef H5 -->
+		<view class="detail-btn-view">
+			<view class="download" @click="download"></view>
+			<!-- #ifdef APP-PLUS -->
+			<view v-if="showBtn" class="setting" @click="setting">设为壁纸</view>
+			<!-- #endif -->
+		</view>
+		<!-- #endif -->
+	</view>
+</template>
+
+<script>
+	import Vue from 'vue';
+	import { mapGetters } from 'vuex';
+	export default Vue.extend( {
+		name: 'collection',
+		data() {
+			return {
+				imgShow: false,
+				index: 0,
+				showBtn: false,
+				screenHeight: 0,
+				imgLength: 0,
+				providerList: [],
+				data: [],
+				detailDec: ""
+			}
+		},
+		onLoad(e) {
+			this.screenHeight = uni.getSystemInfoSync().windowHeight
+      this.data = this.data.concat(this.collections)
+      this.imgLength = this.data.length
+      uni.setNavigationBarTitle({
+				title: "1/" + this.imgLength
+			});
+		},
+		computed: {
+			...mapGetters(['hasCollection', 'collections'])
+		},
+		methods: {
+			download() {
+				uni.downloadFile({
+					url: this.data[this.index],
+					success: (e) => {
+						uni.saveImageToPhotosAlbum({
+							filePath: e.tempFilePath,
+							success: () => {
+								uni.showToast({
+									icon: 'none',
+									title: '已保存到手机相册'
+								})
+							},
+							fail: () => {
+								uni.showToast({
+									icon: 'none',
+									title: '保存到手机相册失败'
+								})
+							}
+						});
+					},
+					fail: (e) => {
+						uni.showModal({
+							content: '下载失败，' + e.errMsg,
+							showCancel: false
+						})
+					}
+				})
+			},
+			//#ifdef APP-PLUS
+			setting() {
+				uni.showToast({
+					icon: 'none',
+					title: '正在设为壁纸'
+				})
+				setTimeout(() => {
+					var WallpaperManager = plus.android.importClass('android.app.WallpaperManager');
+					var Main = plus.android.runtimeMainActivity();
+					var wallpaperManager = WallpaperManager.getInstance(Main);
+					plus.android.importClass(wallpaperManager);
+					var BitmapFactory = plus.android.importClass('android.graphics.BitmapFactory');
+					uni.downloadFile({
+						url: this.data[this.index],
+						success: (e) => {
+							var filePath = plus.io.convertLocalFileSystemURL(e.tempFilePath);
+							var bitmap = BitmapFactory.decodeFile(filePath);
+							try {
+								wallpaperManager.setBitmap(bitmap);
+								uni.showToast({
+									icon: 'none',
+									title: '壁纸设置成功'
+								})
+							} catch (e) {
+								uni.showToast({
+									icon: 'none',
+									title: '壁纸设置失败'
+								})
+							}
+						},
+						fail: () => {
+							uni.showToast({
+								icon: 'none',
+								title: '壁纸设置失败'
+							})
+						}
+					})
+				}, 100)
+			},
+			//#endif
+			swpierChange(e) {
+				this.index = e.detail.current;
+				this.$store.commit('CHECK_COLLECTION', this.data[this.index])
+				uni.setNavigationBarTitle({
+					title: e.detail.current + 1 + '/' + this.imgLength
+				})
+			},
+			preImg(index) {
+				if (this.imgShow) { //防止点击过快导致重复调用 
+					return;
+				}
+				this.imgShow = true;
+				setTimeout(() => {
+					this.imgShow = false;
+				}, 1000)
+				setTimeout(() => {
+					uni.previewImage({
+						current: this.data[index],
+						urls: this.data
+					})
+				}, 150)
+			}
+		}
+	})
+</script>
+
+<style>
+	page {
+		background-color: #000;
+		height: 100%;
+	}
+
+	swiper {
+		flex: 1;
+		width: 750upx;
+		background-color: #000;
+		display: flex;
+		flex-direction: column;
+	}
+
+	swiper-item {
+		display: flex;
+		align-items: center;
+	}
+
+	image {
+		width: 750upx;
+		height: 1125upx;
+	}
+</style>
--- src/pages/my/index.vue
+++ src/pages/my/index.vue
@@ -8,26 +8,9 @@
 			</view>
 		</view>
 		<view class="center-list">
-			<view class="center-list-item border-bottom">
+			<view class="center-list-item border-bottom" @click="goCollection">
 				<text class="list-icon">&#xe60c;</text>
 				<text class="list-text">收藏图片</text>
-				<text class="navigat-arrow">&#xe65e;</text>
-			</view>
-			<view class="center-list-item">
-				<text class="list-icon">&#xe60d;</text>
-				<text class="list-text">收藏图集</text>
-				<text class="navigat-arrow">&#xe65e;</text>
-			</view>
-		</view>
-		<view class="center-list">
-			<view class="center-list-item border-bottom">
-				<text class="list-icon">&#xe60b;</text>
-				<text class="list-text">管理图片</text>
-				<text class="navigat-arrow">&#xe65e;</text>
-			</view>
-			<view class="center-list-item">
-				<text class="list-icon">&#xe61a;</text>
-				<text class="list-text">上传图片</text>
 				<text class="navigat-arrow">&#xe65e;</text>
 			</view>
 		</view>
@@ -82,6 +65,11 @@
 			goCard() {
 				uni.navigateTo({
 					url: './card',
+				});
+			},
+			goCollection() {
+				uni.navigateTo({
+					url: './collection',
 				});
 			}
 		}
