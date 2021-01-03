9ceccdf5255c092dcc0d79fe55657188afaa2984
TwinkleDing <32945061+TwinkleDing@users.noreply.github.com>
2021-01-03 21:59:14 +0800

    Update index.vue

--- src/pages/record/index.vue
+++ src/pages/record/index.vue
@@ -5,10 +5,16 @@
 			:fixed='false'
 		/>
     <view class='content'>
-      <view v-for="item in options" :key="item.prop">
-        <uni-list>
-          <uni-list-item :title="item.label"  :show-switch="true"  @switchChange="switchChange(item)" ></uni-list-item>
-        </uni-list>
+      <view>
+        <view v-for='item in list' :key='item.label'>
+          <uni-list>
+            <uni-list-item
+              :show-switch='true'
+              :switchChecked='item.do'
+              :title='item.label'
+              @switchChange='switchChange($event, item.label)'></uni-list-item>
+          </uni-list>
+        </view>
       </view>
     </view>
   </view>
@@ -16,7 +22,8 @@
 
 <script>
   import mInput from '@/components/m-input.vue';
-  
+  import {setStorage, getStorage} from '@/util/store';
+
   export default {
     name: 'recordIndex',
 		components: {
@@ -26,19 +33,24 @@
       return {
         options: [
           {
-            label: '早点睡'
+            label: '早点睡',
+            do: false
           },
           {
-            label: '少花钱'
+            label: '少花钱',
+            do: false
           },
           {
-            label: '少吃点'
+            label: '少吃点',
+            do: false
           },
           {
-            label: '多动动'
+            label: '多动动',
+            do: false
           },
           {
-            label: '学学习'
+            label: '学学习',
+            do: false
           }
         ],
         list: [],
@@ -46,9 +58,21 @@
         newRecord: ''
       };
     },
+    created() {
+      if(!getStorage('record')) {
+        this.list = this.options;
+      }else {
+        this.list = getStorage('record');
+      }
+    },
     methods: {
-      switchChange(e) {
-        console.log(e);
+      switchChange(e, label) {
+        this.list.map(item => {
+          if(item.label === label) {
+            item.do = e.value;
+          }
+        })
+			  setStorage('record', this.list);
       },
       showAdd() {
         this.addShow = true
@@ -57,5 +81,9 @@
   }
 </script>
 
-<style>
+<style lang="scss">
+.finish {
+  background: #ff80ad;
+  color: #fff;
+}
 </style>
